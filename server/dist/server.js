6e09f61b314ccd2fc2a9af90ee24ce86767f3fe5
Nicholas Costa <nicholascostadev@gmail.com>
2022-05-08 21:34:21 -0300

    Final Commit

--- server/dist/server.js
+++ server/dist/server.js
@@ -4,5 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const routes_1 = require("./routes");
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(routes_1.routes);
 app.listen(3333, () => console.log('Server started on port 3333'));
