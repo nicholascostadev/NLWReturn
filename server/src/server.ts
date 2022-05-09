52e68b96ec398747051f6e9375228838d48ab871
Nicholas Costa <nicholascostadev@gmail.com>
2022-05-08 22:26:38 -0300

    Change migration DB to postgres

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () =>
-	console.log('Server started on port ' + process.env.port || '3333')
+	console.log('Server started on port ' + (process.env.port || '3333'))
 );
