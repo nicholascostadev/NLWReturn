8a9595403b90c3aac8b3f4e3ee0ea99a3fc1099e
Nicholas Costa <nicholascostadev@gmail.com>
2022-05-08 21:16:16 -0300

    Final Commit

--- mobile/src/components/Form/index.tsx
+++ mobile/src/components/Form/index.tsx
@@ -14,91 +14,91 @@ import { Button } from '../Button';
 import { api } from '../../libs/api';
 
 interface Props {
-	feedbackType: FeedbackType;
-	onFeedbackCanceled: () => void;
-	onFeedbackSent: () => void;
+  feedbackType: FeedbackType;
+  onFeedbackCanceled: () => void;
+  onFeedbackSent: () => void;
 }
 
 export function Form({
-	feedbackType,
-	onFeedbackCanceled,
-	onFeedbackSent,
+  feedbackType,
+  onFeedbackCanceled,
+  onFeedbackSent,
 }: Props) {
-	const feedbackTypeInfo = feedbackTypes[feedbackType];
-	const [screenshot, setScreenshot] = useState<string | null>(null);
-	const [isSendingFeedback, setIsSendingFeedback] = useState<boolean>(false);
-	const [comment, setComment] = useState<string>('');
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+  const [isSendingFeedback, setIsSendingFeedback] = useState<boolean>(false);
+  const [comment, setComment] = useState<string>('');
 
-	function handleScreenshot() {
-		captureScreen({
-			format: 'jpg',
-			quality: 0.8,
-		})
-			.then(uri => setScreenshot(uri))
-			.catch(err => console.log(err));
-	}
+  function handleScreenshot() {
+    captureScreen({
+      format: 'jpg',
+      quality: 0.8,
+    })
+      .then(uri => setScreenshot(uri))
+      .catch(err => console.log(err));
+  }
 
-	function handleScreenshotRemove() {
-		setScreenshot(null);
-	}
+  function handleScreenshotRemove() {
+    setScreenshot(null);
+  }
 
-	async function handleSendFeedback() {
-		if (isSendingFeedback) {
-			return;
-		}
+  async function handleSendFeedback() {
+    if (isSendingFeedback) {
+      return;
+    }
 
-		setIsSendingFeedback(true);
-		const screenshotBase64 =
-			screenshot &&
-			(await FileSystem.readAsStringAsync(screenshot, { encoding: 'base64' }));
+    setIsSendingFeedback(true);
+    const screenshotBase64 =
+      screenshot &&
+      (await FileSystem.readAsStringAsync(screenshot, { encoding: 'base64' }));
 
-		try {
-			await api.post('/feedbacks', {
-				type: feedbackType,
-				screenshot: `data:image/png;base64,${screenshotBase64}`,
-				comment,
-			});
+    try {
+      await api.post('/feedbacks', {
+        type: feedbackType,
+        screenshot: `data:image/png;base64,${screenshotBase64}`,
+        comment,
+      });
 
-			onFeedbackSent();
-		} catch (err) {
-			console.log(err);
-			setIsSendingFeedback(false);
-		}
-	}
+      onFeedbackSent();
+    } catch (err) {
+      console.log(err);
+      setIsSendingFeedback(false);
+    }
+  }
 
-	return (
-		<View style={styles.container}>
-			<View style={styles.header}>
-				<TouchableOpacity onPress={onFeedbackCanceled}>
-					<ArrowLeft
-						size={24}
-						weight="bold"
-						color={theme.colors.text_secondary}
-					/>
-				</TouchableOpacity>
-				<View style={styles.titleContainer}>
-					<Image source={feedbackTypeInfo.image} style={styles.image} />
-					<Text style={styles.titleText}>{feedbackTypeInfo.title}</Text>
-				</View>
-			</View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={onFeedbackCanceled}>
+          <ArrowLeft
+            size={24}
+            weight="bold"
+            color={theme.colors.text_secondary}
+          />
+        </TouchableOpacity>
+        <View style={styles.titleContainer}>
+          <Image source={feedbackTypeInfo.image} style={styles.image} />
+          <Text style={styles.titleText}>{feedbackTypeInfo.title}</Text>
+        </View>
+      </View>
 
-			<TextInput
-				multiline
-				style={styles.input}
-				placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo..."
-				placeholderTextColor={theme.colors.text_secondary}
-				autoCorrect={false}
-				onChangeText={setComment}
-			/>
-			<View style={styles.footer}>
-				<ScreenshotButton
-					onTakeShot={handleScreenshot}
-					onRemoveShot={handleScreenshotRemove}
-					screenshot=""
-				/>
+      <TextInput
+        multiline
+        style={styles.input}
+        placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo..."
+        placeholderTextColor={theme.colors.text_secondary}
+        autoCorrect={false}
+        onChangeText={setComment}
+      />
+      <View style={styles.footer}>
+        <ScreenshotButton
+          onTakeShot={handleScreenshot}
+          onRemoveShot={handleScreenshotRemove}
+          screenshot=""
+        />
 
-				<Button onPress={handleSendFeedback} isLoading={isSendingFeedback} />
-			</View>
-		</View>
-	);
+        <Button onPress={handleSendFeedback} isLoading={isSendingFeedback} />
+      </View>
+    </View>
+  );
 }
